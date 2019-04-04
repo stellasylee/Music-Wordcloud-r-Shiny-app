69e801d8e4ba48a2559080bbb7940c777de46ef9
lafarnelli <36866888+lafarnelli@users.noreply.github.com>
2019-04-04 10:49:41 -0500

    Update Cleaning.R

--- Cleaning.R
+++ Cleaning.R
@@ -1,14 +1,21 @@
-library(dplyr)
+#install.packages("dplyr")
+#install.packages("tidyr")
+#install.packages("ggplot2")
+#install.packages("readr")
+library(tidyr)     # contains tools to tidy data
+library(ggplot2)   # for plotting
+library(readr)     # a package for parsing data
+library(dplyr)     # contains functions for data manipulation
 
 #Import Data
-music <- read.csv("https://raw.githubusercontent.com/stellasylee/rShinyProject/master/data/billboard_lyrics_1964-2015.csv")
+music <- read.csv("https://raw.githubusercontent.com/stellasylee/Music-Wordcloud-r-Shiny-app/master/data/billboard_lyrics_1964-2015.csv")
 
-#Filter the Years: 1965-2015
-music <- filter(music, "Year" < 2016)
-music <- filter(music, "Year" > 1964)
+#Filter the Years 
+#music <- filter(music, "Year" < 2019)
+#music <- filter(music, "Year" > 1967)
 
 #Keep to variables we want
-music <- slice(music, 1:5) #Year, Rank, Song, Artist, Lyrics
+#music <- slice(music, 1:5) #Year, Rank, Song, Artist, Lyrics #not working to remove Source
 head(music)
 
 #Need a function to interactively create a dataframe (demoFreq) including word and freq in each column
@@ -18,7 +25,7 @@ head(music)
 
 #Move Lyrics into a separate vector
 
-#Create Decade Variable
+#Create Decades?
 
 #Experiment with colors
 #Experiment with shapes/outlines
