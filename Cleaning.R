342f29b41fd9b8431d955543bb962ce549e8bbee
lafarnelli <36866888+lafarnelli@users.noreply.github.com>
2019-04-04 10:37:03 -0500

    Update Cleaning.R

--- Cleaning.R
+++ Cleaning.R
@@ -1,10 +1,3 @@
-# Install
-#install.packages("tm")  # for text mining
-#install.packages("SnowballC") # for text stemming
-#install.packages("wordcloud") # word-cloud generator 
-#install.packages("wordcloud2") # word-cloud generator 
-#install.packages("RColorBrewer") # color palettes
-
 library(dplyr)
 
 #Import Data
