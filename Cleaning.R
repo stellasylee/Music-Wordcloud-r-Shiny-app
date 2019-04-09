5b125885799a4ee6e5c3d5820da18e627a1232ba
lafarnelli <36866888+lafarnelli@users.noreply.github.com>
2019-04-08 21:19:13 -0500

    Update Cleaning.R

--- Cleaning.R
+++ Cleaning.R
@@ -73,10 +73,11 @@ dtm <- TermDocumentMatrix(docs)
 m <- as.matrix(dtm)
 v <- sort(rowSums(m),decreasing=TRUE)
 d <- data.frame(word = names(v),freq=v)
-set.seed(1234)
-wordcloud(words = d$word, freq = d$freq, min.freq = 1,
-          max.words=200, random.order=FALSE, rot.per=0.35, 
-          colors=brewer.pal(8, "Dark2"))
+return(d)
+#set.seed(1234)
+#wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+#          max.words=200, random.order=FALSE, rot.per=0.35, 
+#          colors=brewer.pal(8, "Dark2"))
 }
 
 newmusic<-filter(music, "Year" > 2012)
