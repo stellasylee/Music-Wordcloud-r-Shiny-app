9f92ee169d1cdf05e940d95b7dada27087beeb22
lafarnelli <36866888+lafarnelli@users.noreply.github.com>
2019-04-08 19:50:13 -0500

    Update Cleaning.R

--- Cleaning.R
+++ Cleaning.R
@@ -2,10 +2,17 @@
 #install.packages("tidyr")
 #install.packages("ggplot2")
 #install.packages("readr")
+install.packages ("tm")
+#install.packages ("wordcloud")
+#install.packages ("RColorBrewer")
+
 library(tidyr)     # contains tools to tidy data
 library(ggplot2)   # for plotting
 library(readr)     # a package for parsing data
 library(dplyr)     # contains functions for data manipulation
+library(tm)
+library(wordcloud)
+library(RColorBrewer)
 
 #Import Data
 music <- read.csv("https://raw.githubusercontent.com/stellasylee/Music-Wordcloud-r-Shiny-app/master/data/billboard_lyrics_1964-2015.csv")
@@ -29,8 +36,36 @@ dim(music)
 #Need a function to interactively create a dataframe (demoFreq) including word and freq in each column
 
 makeCloud <- function(lyricsCol) {
-  wordcloud(paste(lyricsCol, collapse = ''))
-  }
+text<-(paste(lyricsCol, collapse = ''))
+docs <- Corpus(VectorSource(text))
+# Convert the text to lower case
+docs <- tm_map(docs, content_transformer(tolower))
+# Remove numbers
+docs <- tm_map(docs, removeNumbers)
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+# Remove your own stop word
+# specify your stopwords as a character vector
+docs <- tm_map(docs, removeWords, c("blabla1", "blabla2")) 
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+# Text stemming
+# docs <- tm_map(docs, stemDocument)
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+}
+
+newmusic<-filter(music, "Year" > 2012)
+
+makeCloud(newmusic$Lyrics)
 
 #will be applied inside the server
 #Have the default one done beforehand (for all the data)
